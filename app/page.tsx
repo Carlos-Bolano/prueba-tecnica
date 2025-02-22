482862d072691e491c41c2788d3cf58c7ddd5bed
Carlos-Bolano <carlostutos828@gmail.com>
2025-02-21 21:17:13 -0500

    refactor(form): restructure form components
    Improves code organization by separating form components for better maintainability and readability.
    Adds client-side routing and introduces a "Next Step" button to enhance navigation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
-import FormStep1 from "@/components/FormStep1";
+"use client";
+import FormStepOne from "@/components/FormStepOne";
 import FormLayout from "./Form/FormLayout";
+import HeadingsPage from "@/components/HeadingsPage";
+import { useRef } from "react";
+import ButtonsPage from "@/components/ButtonsPage";
 
 export default function Home() {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = () => {
+    if (formRef.current) {
+      formRef.current.requestSubmit();
+    }
+  };
   return (
     <FormLayout>
-      <header className="mt-12 tracking-wide mb-8">
-        <h1 className="text-4xl font-bold text-primary-navyBlue mb-2">Personal info</h1>
-        <p className="text-neutral-coolGray font-ubuntu">
-          Please provide your name, email address, and phone number.
-        </p>
-      </header>
-      <FormStep1 />
+      <main>
+        <HeadingsPage
+          title="Personal info"
+          description="Please provide your name, email address, and phone number."
+        />
+        <FormStepOne formRef={formRef} />
+      </main>
+      <ButtonsPage handleNextStep={handleSubmit} text="Next Step" />
     </FormLayout>
   );
 }
