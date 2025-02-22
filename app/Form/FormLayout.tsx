4319b0f5c0880baaf78d80c00df992a07f333809
Carlos-Bolano <carlostutos828@gmail.com>
2025-02-21 21:17:35 -0500

    refactor(form): improve mobile responsiveness
    Enhances the form layout for better usability on mobile devices by introducing a mobile-specific step selector.
    
    Replaces the sidebar with a more responsive step navigation component, optimizing the user experience on smaller screens.

--- app/Form/FormLayout.tsx
+++ app/Form/FormLayout.tsx
@@ -1,13 +1,19 @@
-import SiderBar from "@/components/SiderBar";
-import React from "react";
+import FormSelectStep from "@/components/FormSelectStep";
+import FormSelectStepMobile from "@/components/FormSelectStepMobile";
+import { cn } from "@/lib/utils";
 
-const FormLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+const FormLayout = ({ children, className }: { children: React.ReactNode; className?: string }) => {
   return (
-    <section className="grid place-items-center min-h-screen py-10 px-10 gap-16 font-ubuntu bg-neutral-magnoliaWhite">
-      <section className="grid grid-cols-1 md:grid-cols-[274px_1fr] bg-white rounded-2xl w-full max-w-[1000px] min-h-[400px] sm:min-h-[500px] md:min-h-[600px] p-4 shadow-blur-shadow">
-        <SiderBar />
-        <article className="w-full h-full px-6 sm:px-10 flex justify-center">
-          <div className="w-full h-full max-h-[600px] overflow-y-auto">{children}</div>
+    <section className="relative flex flex-col items-center min-h-screen px-4 md:py-10 md:px-10 font-ubuntu bg-neutral-magnoliaWhite">
+      <div className="absolute top-0 w-full">
+        <FormSelectStepMobile />
+      </div>
+      <section className="z-10 grid grid-cols-1 md:grid-cols-[274px_1fr] bg-white rounded-2xl w-full max-w-[980px] min-h-min md:h-[600px] p-4 shadow-blur-shadow mt-28 md:m-auto">
+        <FormSelectStep />
+        <article className="w-full h-full px-4 md:px-10 lg:px-24">
+          <div className={cn("w-full h-full flex flex-col flex-1 justify-between", className)}>
+            {children}
+          </div>
         </article>
       </section>
     </section>
