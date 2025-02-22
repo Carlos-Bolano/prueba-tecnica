166e88efe9a10ce25ab19397273f09f0263a3a2d
Carlos-Bolano <carlostutos828@gmail.com>
2025-02-22 14:11:13 -0500

    style(homepage): adjust heading size for better readability
    - Reduces description text size for improved readability on smaller screens.
    - Applies the adjustment conditionally when the user is on the homepage.

--- components/HeadingsPage.tsx
+++ components/HeadingsPage.tsx
@@ -1,13 +1,19 @@
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 import React from "react";
 interface Props {
   title: string;
   description: string;
 }
+
 const HeadingsPage = ({ title, description }: Props) => {
+  const pathname = usePathname();
   return (
     <header className="mt-4 md:mt-12 mb-8">
       <h1 className="text-2xl md:text-4xl font-bold text-primary-navyBlue mb-1 md:mb-2">{title}</h1>
-      <p className="text-neutral-coolGray text-lg">{description}</p>
+      <p className={cn("text-neutral-coolGray text-lg", pathname === "/" && "text-[17.4px]")}>
+        {description}
+      </p>
     </header>
   );
 };
