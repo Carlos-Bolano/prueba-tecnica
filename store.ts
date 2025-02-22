945f5295a4a9be4560a1dcf2cef6e649ddcae71c
Carlos-Bolano <carlostutos828@gmail.com>
2025-02-21 21:15:46 -0500

    feat(form): add reset functionality to form store
    Adds a reset function to the form store, clearing all form data and local storage, improves user experience by allowing users to reset the form to its initial state easily.

--- store.ts
+++ store.ts
@@ -4,12 +4,17 @@ import { FormSchemaType } from "./models/FormSchema";
 
 type FormStepState = Partial<FormSchemaType> & {
   setData: (data: Partial<FormSchemaType>) => void;
+  reset: () => void;
 };
 
 export const useFormStore = create<FormStepState>()(
   persist(
-    (set) => ({
+    (set, get, store) => ({
       setData: (data) => set(data),
+      reset: () => {
+        set(() => ({}));
+        store.persist.clearStorage();
+      },
     }),
     {
       name: "form-storage",
