70b9552d7057bc1c0725b58c4db590b5265c4dc3
Carlos-Bolano <carlostutos828@gmail.com>
2025-02-21 21:19:43 -0500

    feat(form): add thank you page after submission
    Introduces a dedicated page to confirm form completion and thank users, enhancing the user experience.
    Displays a confirmation message and provides contact information for support.

---
+++ app/Form/step-5/page.tsx
@@ -0,0 +1,23 @@
+import FormLayout from "../FormLayout";
+import Image from "next/image";
+
+const FormStepFivePage = () => {
+  return (
+    <FormLayout className="justify-center items-center">
+      <Image
+        className="size-[80px] mx-auto mb-6"
+        src="/images/icon-thank-you.svg"
+        alt="thank you"
+        width={100}
+        height={100}
+      />
+      <h1 className="text-4xl font-bold text-primary-navyBlue mb-4 text-center">Thank you!</h1>
+      <p className="text-neutral-coolGray text-lg text-center">
+        Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need
+        support, please feel free to email us at support@loremgaming.com.
+      </p>
+    </FormLayout>
+  );
+};
+
+export default FormStepFivePage;
