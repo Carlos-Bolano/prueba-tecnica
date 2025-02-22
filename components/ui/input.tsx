5133ca9fbbd11edbb5e9b94212c4ce17c14c410f
Carlos-Bolano <carlostutos828@gmail.com>
2025-02-22 14:05:20 -0500

    style(input): increase input field height for better consistency
    Adjusts input height from 11 to 12 units to enhance visual balance and improve user experience.

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-11 w-full font-ubuntu rounded-md border border-neutral-lightGray bg-transparent px-3 py-4 text-sm font-[700] first-line: shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-coolGray focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-purpleBlue disabled:cursor-not-allowed disabled:opacity-50 md:text-base text-primary-navyBlue input-autofill",
+          "flex h-12 w-full font-ubuntu rounded-md border border-neutral-lightGray bg-transparent px-3 py-4 text-sm font-[700] first-line: shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-coolGray focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-purpleBlue disabled:cursor-not-allowed disabled:opacity-50 md:text-base text-primary-navyBlue input-autofill",
           className
         )}
         ref={ref}
