4edd80ea295c036f14a6d5b1d8c166bcee3fbf5e
Carlos-Bolano <carlostutos828@gmail.com>
2025-02-21 21:19:12 -0500

    refactor(form): improve Step 4 structure and functionality
    Enhances the fourth step by restructuring components for headings, buttons, and specific content.
    Implements navigation to the next step and ensures proper form state management, improving usability.

--- app/Form/step-4/page.tsx
+++ app/Form/step-4/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+import FormLayout from "../FormLayout";
+import { useFormStore } from "@/store";
 
-const FormStep4 = () => {
-  return <div>FormStep4</div>;
+import FoemStepFour from "@/components/StepFour";
+import ButtonsPage from "@/components/ButtonsPage";
+import HeadingsPage from "@/components/HeadingsPage";
+
+const FormStepFourPage = () => {
+  const handleNextStep = () => {
+    useFormStore.getState().reset();
+    window.location.href = "/Form/step-5";
+  };
+  return (
+    <FormLayout>
+      <section>
+        <HeadingsPage
+          title="Finishing up"
+          description="Double-check everything looks OK before confirming."
+        />
+        <FoemStepFour />
+      </section>
+      <ButtonsPage
+        backUrl="/Form/step-3"
+        handleNextStep={handleNextStep}
+        text="Confirm"
+        variant="secondary"
+        goBack={true}
+      />
+    </FormLayout>
+  );
 };
 
-export default FormStep4;
+export default FormStepFourPage;
